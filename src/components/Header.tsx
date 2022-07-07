57ab6a93f9791f09794ce4607512af1da5074d04
Guilherme Ribeiro <augusto.gui.2208@gmail.com>
2022-07-07 16:51:53 -0300

    botao de voltar para home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,47 @@
-import { Flex, Image } from '@chakra-ui/react'
-export function Header() {
+import { Flex, IconButton, Image } from '@chakra-ui/react'
+import Link from 'next/link'
+
+interface HeaderProps {
+  buttonBackHome?: boolean
+}
+
+export function Header({ buttonBackHome = false }: HeaderProps) {
   return (
     <Flex
       py={['4', '4', '6']}
       justifyContent="center"
+      align="center"
       as="header"
       w="100%"
       maxW={1430}
       mx="auto"
     >
-      <Image src="Logo.png" alt="Logo da empresa worldTrip" />
+      {buttonBackHome && (
+        <Link href="/">
+          <IconButton
+            marginRight="auto"
+            variant="unstyled"
+            aria-label="Cidades +100"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            icon={
+              <Image
+                src="./../botao_voltar.png"
+                alt="Boão de voltar"
+                justifySelf="start"
+              />
+            }
+          />
+        </Link>
+      )}
+
+      <Image
+        src="./../Logo.png"
+        alt="Logo da empresa worldTrip"
+        marginRight="auto"
+        marginLeft={buttonBackHome ? '' : 'auto'}
+      />
     </Flex>
   )
 }
